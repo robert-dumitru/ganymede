239295320fd5371984130f7acb8622dd07b62b48
robertdumitru <robert.dumitru@uni.minerva.edu>
2022-08-22 19:11:44 -0300

    added multiple requirement files

---
+++ ec2_server.py
@@ -0,0 +1,7 @@
+"""
+This is a temporary workaround to get around the docker chromium limitation on lambda. If you're using this, clone
+this repo, install the requirements, and run start the uvicorn server with the following command: ec2_server:main
+"""
+
+import fastapi
+
