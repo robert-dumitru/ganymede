3433cd79afd41aafdf5144e7afd70523fe2c9a9a
robertdumitru <robert.dumitru@uni.minerva.edu>
2022-08-22 20:32:43 -0300

    added ec2 script

--- ec2_server.py
+++ ec2_server.py
@@ -1,7 +1,19 @@
 """
 This is a temporary workaround to get around the docker chromium limitation on lambda. If you're using this, clone
-this repo, install the requirements, and run start the uvicorn server with the following command: ec2_server:main
+this repo, install the requirements, and start the uvicorn server with the following command: ec2_server:app
 """
 
-import fastapi
+import logging
+import subprocess
+from fastapi import FastAPI, Request
 
+app = FastAPI()
+
+
+@app.post("/")
+async def forward_tb_request(request: Request) -> None:
+    logging.debug(f"Captured request: {request}")
+    event: dict = {"body": request.json()}
+    context: dict = {}
+    subprocess.Popen(["python", "-c", f"import app.process_messages.handler as handler; handler({event}, {context})"])
+    return
