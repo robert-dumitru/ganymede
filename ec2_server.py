a39b4d3c8bde80249b38102b9adc4503c6334e16
robertdumitru <robert.dumitru@uni.minerva.edu>
2022-08-22 21:24:42 -0300

    changed to polling instead of webhook

--- ec2_server.py
+++ ec2_server.py
@@ -1,19 +1,12 @@
 """
 This is a temporary workaround to get around the docker chromium limitation on lambda. If you're using this, clone
-this repo, install the requirements, and start the uvicorn server with the following command: ec2_server:app
+this repo, install the requirements, and start the python script. Remember to deactivate webhooks.
 """
 
-import logging
-import subprocess
-from fastapi import FastAPI, Request
+import app.process_messages as pm
+import telebot
 
-app = FastAPI()
+tb: telebot.TeleBot = pm.tb
+tb.infinity_polling()
 
 
-@app.post("/")
-async def forward_tb_request(request: Request) -> None:
-    logging.debug(f"Captured request: {request}")
-    event: dict = {"body": request.json()}
-    context: dict = {}
-    subprocess.Popen(["python", "-c", f"import app.process_messages.handler as handler; handler({event}, {context})"])
-    return
